a36c197b1ee5b60c77e2fd26bf4e44ed238856bc
Stefano <steframony@gmail.com>
2024-03-02 19:07:24 +0100

    Pagamenti Modifica

---
+++ web/src/main/java/Inap/web/Entity/PagamentiCorsi.java
@@ -0,0 +1,32 @@
+package Inap.web.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class PagamentiCorsi {
+    @GeneratedValue
+    @Id
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @ManyToOne(optional = false)
+    private Utente utente;
+
+    public Utente getUtente() {
+        return utente;
+    }
+
+    public void setUtente(Utente utente) {
+        this.utente = utente;
+    }
+}
--- web/src/main/java/Inap/web/Entity/Utente.java
+++ web/src/main/java/Inap/web/Entity/Utente.java
@@ -1,4 +1,45 @@
 package Inap.web.Entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+import java.util.Collection;
+
+@Entity
 public class Utente {
+    @GeneratedValue
+    @Id
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @OneToMany(mappedBy = "utente")
+    private Collection<PagamentiAnnuali> pagamentiAnnuali;
+
+    public Collection<PagamentiAnnuali> getPagamentiAnnuali() {
+        return pagamentiAnnuali;
+    }
+
+    public void setPagamentiAnnuali(Collection<PagamentiAnnuali> pagamentiAnnuali) {
+        this.pagamentiAnnuali = pagamentiAnnuali;
+    }
+
+    @OneToMany(mappedBy = "utente")
+    private Collection<PagamentiCorsi> pagamentiCorsi;
+
+    public Collection<PagamentiCorsi> getPagamentiCorsi() {
+        return pagamentiCorsi;
+    }
+
+    public void setPagamentiCorsi(Collection<PagamentiCorsi> pagamentiCorsi) {
+        this.pagamentiCorsi = pagamentiCorsi;
+    }
 }
